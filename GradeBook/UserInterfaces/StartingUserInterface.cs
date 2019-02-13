7491ffd0cd2607291ad435253a3727536688a7e4
Nemanja11 <47555562+Nemanja11@users.noreply.github.com>
2019-02-13 11:22:34 +0100

    Override

--- GradeBook/UserInterfaces/StartingUserInterface.cs
+++ GradeBook/UserInterfaces/StartingUserInterface.cs
@@ -42,11 +42,8 @@ namespace GradeBook.UserInterfaces
             }
             var name = parts[1];
             var type = parts[2].ToLower();
-            if (type == "")
-            {
-                return;
-            }
-            BaseGradeBook gradeBook = new BaseGradeBook(name);
+         
+            BaseGradeBook gradeBook=null;
             if(type == "standard")
 
              gradeBook=new StandardGradeBook(name);
